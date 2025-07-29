eeeed3d6ca209b53e4cd6ddf10eec65bfe3c3280
Tenzin Kalsang <109454940+fawfol@users.noreply.github.com>
2025-07-29 21:05:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+import Todo from "./components/Todo/Todo";
+import "./global.css"; // Your global styles
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
