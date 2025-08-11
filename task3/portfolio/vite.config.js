b217f2354601a2080934b4da116d76789c41df47
Tenzin Kalsang <kalsangkalsang5@gmail.com>
2025-08-11 14:22:15 +0530

    task 3 basic portfolio with react route to routing pages

--- task3/portfolio/vite.config.js
+++ task3/portfolio/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
 })
+
+/*// vite.config.js
+export default {
+  base: '/port/', // subfolder path
+}*/
+
+
