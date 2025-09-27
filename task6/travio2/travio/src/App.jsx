d2df89557d5cca3637596bf5bfbf10137bcf077a
Tenzin Kalsang <kalsangkalsang5@gmail.com>
2025-09-27 11:54:12 +0530

    landign page file ipload

---
+++ task6/travio2/travio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthPage from './components/AuthPage';
+import './App.css'; // Import the main CSS file
+
+function App() {
+  return (
+    <div className="App">
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
